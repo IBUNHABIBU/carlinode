72642b83bf62f8816155e21bf01e8def5b0c402a
Ibunhabibu <Ibunhabib@gmail.com>
2023-04-19 23:43:00 +0300

    Add skills and project controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,12 @@
 class Project < ApplicationRecord
+    has_one_attached :screenshot
+  validates :title, :github, :demo, :languages, :screenshot, presence: true
+
+  def imageUrl
+    if screenshot.attached?
+      Rails.application.routes.url_helpers.url_for(screenshot)
+      # Rails.application.routes.url_helpers.rails_blob_path(screenshot)
+
+    end
+  end
 end
